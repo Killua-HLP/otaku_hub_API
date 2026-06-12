c0c2792e6a010ab2c15b2465720ca980e4fd8f2c
Killua HLP <killuahlp125@gmail.com>
2026-06-12 13:18:40 +0900

    feat: fix

--- database.py
+++ database.py
@@ -9,7 +9,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 load_dotenv()
 DB_URL = os.getenv(
-    "DB_URL", "fpostgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+    f"DB_URL", "postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
 if not os.getenv("DB_URL"):
     print("Environment variable 'DB_URL' not found in .env, using default fallback.")
 
