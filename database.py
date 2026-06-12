f216727b19a2b243db1337dd74062e762724f425
Killua HLP <killuahlp125@gmail.com>
2026-06-12 13:14:38 +0900

    feat: fix workflow

--- database.py
+++ database.py
@@ -5,10 +5,11 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 load_dotenv()
 DB_URL = os.getenv(
     "DB_URL", "fpostgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
-
 if not os.getenv("DB_URL"):
     print("Environment variable 'DB_URL' not found in .env, using default fallback.")
 
